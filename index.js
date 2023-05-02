e8de9606cda4314567e81e387c728ff56988b561
Durga Prasad <durgaamrapalli0801@gmail.com>
2023-05-02 19:52:19 +0530

    updated Files

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
-const express = require("express")
-const bodyparser = require("body-Parser")
+const express = require("express");
+const bodyparser = require("body-parser");
+const keys = require('dotenv').config();
 const app = express()
 const http = require("https")
 app.set("view engine","ejs")
 app.use(express.static("files"))
 app.use(bodyparser.urlencoded({extended:true}))
+
+const appid = process.env.API_KEY
 app.get("/",function(req,res){
     res.render("index",{
         temp:"-",
         wind:"-",
+        humidity:"-",
         visibility:"-",
         city:"-"
     })
 })
 app.post("/",function(req,res){
-    const city=req.body.City
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=ef30456bf732b291363039c65c3d307c&units=metric"
+    const city=req.body.City;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appid}&units=metric`;
     http.get(url,function(response){
         response.on("data",function(data){
-            const weatherData = JSON.parse(data)
+            const weatherData = JSON.parse(data);
             if(weatherData.cod===200){
                 const temp = weatherData.main.temp
                 const weatherDes = weatherData.weather[0].description
                 const icon = weatherData.weather[0].icon
                 const wind = weatherData.wind.speed
+                const humidity = weatherData.main.humidity
                 const visibility = weatherData.visibility
+                const name = weatherData.name
                 const imgurl =  "https://openweathermap.org/img/wn/"+icon+"@2x.png"
                 res.render("index",{
                     temp:temp,
                     wind:wind,
+                    humidity:humidity,
                     visibility:visibility,
-                    city:city
+                    city:name
                 })
             }
             else{
@@ -38,6 +45,7 @@ app.post("/",function(req,res){
                     temp:"-",
                     wind:"-",
                     visibility:"-",
+                    humidity:"-",
                     city:"City Not Found"
                 })
             }
